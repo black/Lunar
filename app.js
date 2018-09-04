3bfcdb33724a387578774fbc00b66cc935923d45
Λ B H I N Λ V <ixd.abhi@gmail.com>
2018-09-04 14:26:35 +0530

    Image Viewer
    Need to fix starting load image and add keypress support for naivgation

---
+++ app.js
@@ -0,0 +1,17 @@
+const electron = require('electron'),
+    url = require('url'),
+    path = require('path');
+
+const { app, BrowserWindow } = electron;
+
+let mainWindow;
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow();
+    mainWindow.setMenu(null);
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+});
---
+++ main.js
@@ -0,0 +1,62 @@
+(function() {
+    var { dialog } = require('electron').remote;
+    var path = require('path');
+    var fs = require('fs');
+
+    let index = 0;
+    let dirArray = [];
+    let folder = "";
+    let imgContainer = $('<img>');
+    let divContainer = $('#preview');
+
+    $('#readfile').on('click', function() {
+        dialog.showOpenDialog({
+            title: "Open Folder",
+            filters: [{
+                name: 'Images',
+                extensions: ['png', 'jpg', 'jpeg', 'gif']
+            }]
+        }, function(fileNames) {
+            if (fileNames === undefined) {
+                console.log("no file is selected");
+                return;
+            } else {
+                console.log('FN', fileNames[0]);
+                let folderPath = path.dirname(fileNames[0]);
+                console.log('FP', folderPath);
+                folder = folderPath;
+                fs.readdir(folderPath, function(err, dir) {
+                    dirArray = dir;
+                    for (let i = 0; i < dir.length; i++) {
+                        dirArray[i] = fs.readFileSync(folder + '/' + dir[i]).toString('base64');;
+                    }
+                });
+            }
+        });
+    });
+
+    $('#prev').on('click', function() {
+        if (index > 0) {
+            index--;
+            displayImages(dirArray[index]);
+        } else {
+            index = dirArray.length;
+        }
+    });
+    $('#next').on('click', function() {
+        if (index < dirArray.length) {
+            index++;
+            displayImages(dirArray[index]);
+        } else {
+            index = 0;
+        }
+    });
+
+    function displayImages(loadImage) {
+        imgContainer.attr({
+        	"class":'preview-images',
+        	"src": "data:image/png;base64," + loadImage
+        });
+        divContainer.empty().append(imgContainer);
+    }
+})();
